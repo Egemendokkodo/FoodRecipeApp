2f50c86bdad40fbc3b03583c9283e97e5a40ed2f
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2023-04-04 22:50:00 +0300

    added categories recycler to main page

--- app/src/main/java/com/uygulamalarim/foodrecipeapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/uygulamalarim/foodrecipeapp/Fragments/HomeFragment.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.uygulamalarim.foodrecipeapp.Adapter.CategoryAdapter;
+import com.uygulamalarim.foodrecipeapp.Model.CategoryModel.CategoryDomain;
 import com.uygulamalarim.foodrecipeapp.R;
+
+import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerViewCategory;
+    private RecyclerView.Adapter adapter;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,5 +41,25 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        recyclerViewCategory(view);
+
     }
+    private void recyclerViewCategory(View view) {
+        // this is categorylist in the main page
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategory=view.findViewById(R.id.categoriesRecycler);
+        recyclerViewCategory.setLayoutManager(linearLayoutManager);
+
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Main Dishes","pic1"));
+        category.add(new CategoryDomain("Salads","pic2"));
+        category.add(new CategoryDomain("Desserts","pic3"));
+        category.add(new CategoryDomain("Drinks","pic4"));
+        category.add(new CategoryDomain("Appetiser","pic5"));
+        adapter=new CategoryAdapter(category);
+        recyclerViewCategory.setAdapter(adapter);
+    }
+
+
 }
